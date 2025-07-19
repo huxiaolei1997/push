0e4a65e1347f35d88fd75870a54fa2b098aac487
huxiaolei1997 <huxiaolei1997@gmail.com>
2025-07-19 11:29:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,30 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 today = datetime.now()
 
 # 微信公众测试号ID和SECRET
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
+app_id = 'wxab190723497fc82b'
+app_secret = 'c14d943f9bcd752943c1d10fc6b60776'
 
 # 可把os.environ结果替换成字符串在本地调试
-user_ids = os.environ["USER_ID"].split(',')
-template_ids = os.environ["TEMPLATE_ID"].split(',')
-citys = os.environ["CITY"].split(',')
-solarys = os.environ["SOLARY"].split(',')
-start_dates = os.environ["START_DATE"].split(',')
-birthdays = os.environ["BIRTHDAY"].split(',')
+# user_ids = os.environ["USER_ID"].split(',')
+# template_ids = os.environ["TEMPLATE_ID"].split(',')
+# citys = os.environ["CITY"].split(',')
+# solarys = os.environ["SOLARY"].split(',')
+# start_dates = os.environ["START_DATE"].split(',')
+# birthdays = os.environ["BIRTHDAY"].split(',')
 
+user_ids = 'onbfH6vz6u3fWReh0wPjfAdny2yw'.split(',')
+template_ids = '6G6Szvcoqk19_wrimh6Sf97DoNBdYe4Trmr7uL_-zMw'.split(',')
+citys = '330108'.split(',')
+start_dates = '2025-05-18'.split(',')
+birthdays = '04-16'.split(',')
 
 # 获取天气和温度
 def get_weather(city):
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    url = "https://restapi.amap.com/v3/weather/weatherInfo?key=31a3754cb9daaf678f8e972bf3dea5cc&city=" + city + "&extensions=base&output=json"
     res = requests.get(url).json()
-    weather = res['data']['list'][0]
-    return weather['weather'], math.floor(weather['temp'])
+    weather = res['lives'][0]
+    return weather['weather'], weather['temperature_float'], weather['city']
+    # return '晴', '23'
 
 
 # 当前城市、日期
@@ -76,21 +82,16 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 
 for i in range(len(user_ids)):
-    wea, tem = get_weather(citys[i])
+    wea, tem, city_name = get_weather(citys[i])
     cit, dat = get_city_date(citys[i])
     data = {
-        "date": {"value": "今日日期：{}".format(dat), "color": get_random_color()},
-        "city": {"value": "当前城市：{}".format(cit), "color": get_random_color()},
-        "weather": {"value": "今日天气：{}".format(wea), "color": get_random_color()},
-        "temperature": {"value": "当前温度：{}".format(tem), "color": get_random_color()},
-        "love_days": {"value": "今天是你们在一起的第{}天".format(get_count(start_dates[i])), "color": get_random_color()},
-        "birthday_left": {"value": "距离她的生日还有{}天".format(get_birthday(birthdays[i])), "color": get_random_color()},
-        "solary": {"value": "距离发工资还有{}天".format(get_solary(solarys[i])), "color": get_random_color()},
+        "date": {"value": "{}".format(dat), "color": get_random_color()},
+        "city": {"value": "{}".format(city_name), "color": get_random_color()},
+        "weather": {"value": "{}".format(wea), "color": get_random_color()},
+        "temperature": {"value": "{}".format(tem), "color": get_random_color()},
+        "birthday_left": {"value": "{}".format(get_birthday(birthdays[i])), "color": get_random_color()},
+        "love_days": {"value": "{}".format(get_count(start_dates[i])), "color": get_random_color()},
         "words": {"value": get_words(), "color": get_random_color()}
     }
-    if get_birthday(birthdays[i]) == 0:
-        data["birthday_left"]['value'] = "今天是她的生日哦，快去一起甜蜜吧"
-    if get_solary(solarys[i]) == 0:
-        data["solary"]['value'] = "今天发工资啦，快去犒劳一下自己吧"
     res = wm.send_template(user_ids[i], template_ids[i], data)
     print(res)
